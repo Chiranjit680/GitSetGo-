cf1c1476b309b7cb8b9d52e2de07bad696d7d55f
Chiranjit680 <chiranjitsaha680@gmail.com>
2025-04-10 14:43:31 +0530

    initial setup front end installed react-hot-toast react-router-dom react-icons

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'  // <-- Very Important!
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hello from home page</p>
+    </>
+  )
+}
+
+export default App
